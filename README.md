ed89e01d5f51b861f681dc6f4842794d5404f806
Garvit Jain <garvit.jain@aptportfolio.com>
2020-04-21 17:30:14 +0530

    Updating README and comments

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # lords_mobile
+
+This repo will house python scripts for automating tasks in the LORDS MOBILE mobile game. Have started with pyautogui module to automate clicks on the screen.The first script hit_monster.py has been developed on my mac and hence hard coded values can be found. 
+
+INSPIRATION: During guild fest, we often land at tasks like hit monsters. I use this opportunity to increase EXP of heroes which haven't maxed out yet...which means 8-10 hits per lvl1 monster. Doing this manually seems mundane and hence the source of inspiration.
+
+REQUIREMENTS:
+1. python3 installation : https://realpython.com/installing-python/
+2. pyautogui module installation : In terminal enter the following command
+    >> pip install PyAutoGUI
+3. Bluestacks installation : https://www.bluestacks.com/
+    - Android gaming platform for PC
+
+PREREQUISITES:
+1. Launch Bluestacks
+2. Launch Lords Mobile app
+3. Run the python script in a terminal. 
+4. Switch to the game window. The scripts have buffer at start to allow switching from terminal to game window.
+Tip : I do not resize/move the bluestack window for ease of scripts to work. Eg. As per my mac, the screen coordinates for the center of the bluestacks window is (x=850, y=525). I make sure to bring turf item at these coordinates, to ensure the script works seemlessly every time.
+
+TODO:
+- Send help automation : If a guild member gets a task to send helps, a script should continuously put a building in upgrade, ask for help and cancel upgrade repeatedly. Planning to use pyautogui again.
--- hit_monster.py
+++ hit_monster.py
@@ -1,39 +1,44 @@
-# developed for python3 on mac
-# running Lords Mobile on BlueStacks software | I do NOT move/resize the bluestacks window
-import pyautogui 
+"""
+hit_monster.py
+
+Inputs:
+    Screen coordinates of the monster i.e. screen coordinates of the center of the bluestacks window | Screen coordinates can be found by Shift+Command+4
+    Repeat interval = (Time to travel to monster * 2) + 10 | +10 to take into account time spent at monster and safe buffer time
+"""
+import pyautogui
 import time
 
-# this is to have time to bring the game window on the top
+# this is to have time to bring the game window on the top/front
 print("Waiting 5 sec")
 time.sleep(5)
 
 # number of times you wish/know to hit the monster
-number_of_hits=3
+number_of_hits = 3
 
 while(number_of_hits):
     # monster coordinates (not turf coordinates, screen coordinates)
     # IMPORTANT : For first time, once your heroes start and reach the monster, record the screen coordinates.
     #     On mac, press Shift+Command+4 to get the crosshair. Take it to the monster position.
-    # Fo my mac, x=850, y=525 are the screen coordinates 
+    # Fo my mac, x=850, y=525 are the screen coordinates
     # For subsequent runs, scroll in the game to bring your monster to above screen coordinates before starting the script.
     pyautogui.moveTo(850, 525)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
     time.sleep(1)
 
-    # screen coordinates for 1 hit 
-    # Fo my mac, x=930, y=630 are the screen coordinates 
+    # screen coordinates for 1 hit
+    # Fo my mac, x=930, y=630 are the screen coordinates
     # These coordinates once configured, won't change
-    pyautogui.moveTo(930,630)
+    pyautogui.moveTo(930, 630)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
     time.sleep(1)
 
     # screen coordinates for sending heroes | Assuming you dont want to change heroes
-    # Fo my mac, x=1212, y=730 are the screen coordinates 
+    # Fo my mac, x=1212, y=730 are the screen coordinates
     # These coordinates once configured, won't change
     pyautogui.moveTo(1212, 730)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
     time.sleep(30)
-    number_of_hits-=1
+    number_of_hits -= 1
